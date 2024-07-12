9be68230a5d3eae80c70b1e04919eeb362642b03
SidShi <shitianyisid@gmail.com>
2024-07-11 17:05:22 -0700

    Debugging

--- adi.c
+++ adi.c
@@ -1786,6 +1786,9 @@ void fadi_ttsvd(superlu_dist_options_t options, int d, int_t *ms, int_t *nnzs, d
     if (grid1->iam != -1) {
         TTcores_global = (double **) SUPERLU_MALLOC((d-2)*sizeof(double*));
 
+        printf("Proc %d in Grid 1 starts fadi_col for the first dimension!\n", grid1->iam);
+        fflush(stdout);
+
         fadi_col(options, ms[0], nnzs[0], nzvals[0], rowinds[0], colptrs[0], grid1, Us[0], locals[0], 
             ps[0], qs[0], ls[0], tol, &(TTcores[0]), nrhss[0], &rr1);
         rs[0] = rr1;
